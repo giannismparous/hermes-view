0c91effc7907f0052fc58ece2b5d095a9d09f1f9
Giannis Mparous <p3190131@aueb.gr>
2024-01-14 15:48:52 +0200

    Fixed imagescroller images proportions for every device and integrated the sample project

--- src/App.js
+++ src/App.js
@@ -14,6 +14,21 @@ import Projects from "./components/Projects";
 import ProjectDetail from "./components/ProjectDetail";
 import NavigationBar from "./components/NavigationBar";
 
+function SamplePage() {
+  return (
+    <div>
+      {/* You can use an iframe or another component to embed your 3D Vista project */}
+      <iframe
+        title="3D Vista Project"
+        src="/samples/sample1/index.htm"  // Update the path accordingly
+        width="100%"
+        height="1000px"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
@@ -26,6 +41,7 @@ function App() {
         {/* <Route path="/projects" element={<Projects />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/projects/:projectId" element={<ProjectDetail />} /> */}
+        <Route path="/sample" element={<SamplePage />} />
       </Routes>
     </Router>
   );
