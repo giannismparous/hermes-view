818f5540df559e379c146adac0e5a5d156d59c27
Giannis Mparous <p3190131@aueb.gr>
2024-01-27 17:25:52 +0200

    scrollbar is disabled when menu is open

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
@@ -7,13 +7,26 @@ import '../styles/Logo.css'; // Import the styles for the Logo
 
 const logoImg='../logo/hermes-view-logo.png';
 
-
 function NavigationBar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  useEffect(() => {
+    // Add or remove the 'no-scroll' class based on the menuOpen state
+    if (menuOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+
+    // Cleanup the class on component unmount
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, [menuOpen]);
 
   return (
     <nav>
@@ -30,7 +43,7 @@ function NavigationBar() {
             <li><Link to="/contact" onClick={toggleMenu}>Contact Us</Link></li>
             <li className="social-media-list">
               <a href="https://www.facebook.com/profile.php?id=61555932080153" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-                <FontAwesomeIcon icon={faFacebookF} size="lg" />
+                <FontAwesomeIcon icon={faFacebookF}  />
               </a>
               <a href="https://www.instagram.com/hermes_view/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
                 <FontAwesomeIcon icon={faInstagram} />
