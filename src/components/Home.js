3d6b99b93e41ada8af3ba8ad23ff954bdd19befd
Giannis Mparous <p3190131@aueb.gr>
2024-03-29 16:44:56 +0200

    major website alterations

--- src/components/Home.js
+++ src/components/Home.js
@@ -172,7 +172,7 @@ const AnimatedParagraph = ({ children, className }) => {
         <div className="centered-header">
           <h2>Projects</h2>
         </div>
-        { <div className="sample-display-container">
+        {/* {!isMobile && <div className="sample-display-container">
           <div className="sample-item">
             <SampleDisplay modelPath={"https://giannismparous.github.io/vr_1/"} device={"iphone"} />
             <div className="sample-info">
@@ -212,50 +212,50 @@ const AnimatedParagraph = ({ children, className }) => {
               <AnimatedParagraph>Explore this enormous central shopping center. Have a glance at the dining and fashion options.</AnimatedParagraph>
               <Link to="/projects/3" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
-          </div>
-        </div>}
-        {/* {isMobile && <div className="sample-display-container">
-          <div className="sample-item">
-            <div className="sample-info">
-              <AnimatedHeading>Kallithea Property</AnimatedHeading>
-              <AnimatedParagraph>A petite luxury apartement in Athens, Kallithea.</AnimatedParagraph>
-              <Link to="/projects/1" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
-            </div>
-            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_1/"} device={"iphone"} />
-          </div>
-          <div className="sample-item">
-            <div className="sample-info">
-              <AnimatedHeading>Finders, Kato Patisia</AnimatedHeading>
-              <AnimatedParagraph>A recently built and semi-furnished luxury apartment in Kato Patisia. An assistan provides all the information needed for the exploration of the property.</AnimatedParagraph>
-              <Link to="/projects/5" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
-            </div>
-            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_5/"} device={"ipad"} />
-          </div>
-          <div className="sample-item">
-            <div className="sample-info">
-              <AnimatedHeading>Madania Cafe Bar</AnimatedHeading>
-              <AnimatedParagraph>An alternative cafe bistro in the area of Dafne. Take a look at its baroque design and festive atmosphere.</AnimatedParagraph>
-              <Link to="/projects/4" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
-            </div>
-            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_4/"} device={"imac"} />
-          </div>
-          <div className="sample-item">
-            <div className="sample-info">
-              <AnimatedHeading>Neos Kosmos AirBnB</AnimatedHeading>
-              <AnimatedParagraph>A neat spacious airbnb property read to accomodate families right in the heart of Athens.</AnimatedParagraph>
-              <Link to="/sample" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
-            </div>
-            <SampleDisplay modelPath={"/samples/sample1/index.htm"} device={"ipad"} />
-          </div>
-        <div className="sample-item">
-            <div className="sample-info">
-              <AnimatedHeading>Athens Metro Mall</AnimatedHeading>
-              <AnimatedParagraph>Explore this enormous central shopping center. Have a glance at the dining and fashion options.</AnimatedParagraph>
-              <Link to="/projects/3" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
-            </div>
-            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_3/"} device={"iphone"} />
           </div>
         </div>} */}
+        {isMobile && <div className="sample-display-container">
+          <div className="sample-item">
+            <div className="sample-info">
+              <AnimatedHeading>Kallithea Property</AnimatedHeading>
+              <AnimatedParagraph>A petite luxury apartement in Athens, Kallithea.</AnimatedParagraph>
+              <Link to="/projects/1" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
+            </div>
+            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_1/"} device={"iphone"} />
+          </div>
+          <div className="sample-item">
+            <div className="sample-info">
+              <AnimatedHeading>Finders, Kato Patisia</AnimatedHeading>
+              <AnimatedParagraph>A recently built and semi-furnished luxury apartment in Kato Patisia. An assistan provides all the information needed for the exploration of the property.</AnimatedParagraph>
+              <Link to="/projects/5" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
+            </div>
+            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_5/"} device={"ipad"} />
+          </div>
+          <div className="sample-item">
+            <div className="sample-info">
+              <AnimatedHeading>Madania Cafe Bar</AnimatedHeading>
+              <AnimatedParagraph>An alternative cafe bistro in the area of Dafne. Take a look at its baroque design and festive atmosphere.</AnimatedParagraph>
+              <Link to="/projects/4" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
+            </div>
+            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_4/"} device={"imac"} />
+          </div>
+          <div className="sample-item">
+            <div className="sample-info">
+              <AnimatedHeading>Neos Kosmos AirBnB</AnimatedHeading>
+              <AnimatedParagraph>A neat spacious airbnb property read to accomodate families right in the heart of Athens.</AnimatedParagraph>
+              <Link to="/sample" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
+            </div>
+            <SampleDisplay modelPath={"/samples/sample1/index.htm"} device={"ipad"} />
+          </div>
+        <div className="sample-item">
+            <div className="sample-info">
+              <AnimatedHeading>Athens Metro Mall</AnimatedHeading>
+              <AnimatedParagraph>Explore this enormous central shopping center. Have a glance at the dining and fashion options.</AnimatedParagraph>
+              <Link to="/projects/3" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
+            </div>
+            <SampleDisplay modelPath={"https://giannismparous.github.io/vr_3/"} device={"iphone"} />
+          </div>
+        </div>}
       </section>
         <div className="invis-container"/>
         <section className="questions container-4">
