2a16f7622722ad0a83b3b3333ea9367a0ae5f6d0
Giannis Mparous <p3190131@aueb.gr>
2024-03-30 13:34:55 +0200

    video thumbnail

--- src/components/Home.js
+++ src/components/Home.js
@@ -217,30 +217,30 @@ const AnimatedParagraph = ({ children, className }) => {
         {
         // isMobile && 
         <div className="sample-display-container">
-          {/* <div className="sample-item">
+          <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Kallithea Property</AnimatedHeading>
               <AnimatedParagraph>A petite luxury apartement in Athens, Kallithea.</AnimatedParagraph>
               <Link to="/projects/1" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"https://giannismparous.github.io/vr_1/"} device={"iphone"} />
-          </div> */}
-          {/* <div className="sample-item">
+          </div>
+          <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Finders, Kato Patisia</AnimatedHeading>
               <AnimatedParagraph>A recently built and semi-furnished luxury apartment in Kato Patisia. An assistan provides all the information needed for the exploration of the property.</AnimatedParagraph>
               <Link to="/projects/5" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"https://giannismparous.github.io/vr_5/"} device={"ipad"} />
-          </div> */}
-          {/* <div className="sample-item">
+          </div>
+          <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Madania Cafe Bar</AnimatedHeading>
               <AnimatedParagraph>An alternative cafe bistro in the area of Dafne. Take a look at its baroque design and festive atmosphere.</AnimatedParagraph>
               <Link to="/projects/4" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"https://giannismparous.github.io/vr_4/"} device={"imac"} />
-          </div> */}
+          </div>
           <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Neos Kosmos AirBnB</AnimatedHeading>
@@ -249,14 +249,14 @@ const AnimatedParagraph = ({ children, className }) => {
             </div>
             <SampleDisplay modelPath={"/samples/sample1/index.htm"} device={"ipad"} />
           </div>
-        {/* <div className="sample-item">
+        <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Athens Metro Mall</AnimatedHeading>
               <AnimatedParagraph>Explore this enormous central shopping center. Have a glance at the dining and fashion options.</AnimatedParagraph>
               <Link to="/projects/3" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"https://giannismparous.github.io/vr_3/"} device={"iphone"} />
-          </div> */}
+          </div>
         </div>}
       </section>
         <div className="invis-container"/>
