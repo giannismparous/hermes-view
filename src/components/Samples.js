279936e85845460768613bffa9cbdc73576c041c
Giannis Mparous <p3190131@aueb.gr>
2024-03-30 13:37:24 +0200

    video thumbnail

--- src/components/Samples.js
+++ src/components/Samples.js
@@ -127,30 +127,30 @@ return (
         </h2>
       </div>
       <div className="sample-display-container">
-          {/* <div className="sample-item">
+          <div className="sample-item">
             <SampleDisplay modelPath={"/projects/1"} device={"iphone"} />
             <div className="sample-info">
               <AnimatedHeading>Kallithea Property</AnimatedHeading>
               <AnimatedParagraph>A petite luxury apartement in Athens, Kallithea.</AnimatedParagraph>
               <Link to="/projects/1" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
-          </div> */}
-          {/* <div className="sample-item">
+          </div>
+          <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Finders, Kato Patisia</AnimatedHeading>
               <AnimatedParagraph>A recently built and semi-furnished luxury apartment in Kato Patisia. An assistan provides all the information needed for the exploration of the property.</AnimatedParagraph>
               <Link to="/projects/5" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"/projects/5"} device={"ipad"} />
-          </div> */}
-          {/* <div className="sample-item">
+          </div>
+          <div className="sample-item">
             <SampleDisplay modelPath={"/projects/4"} device={"imac"} />
             <div className="sample-info">
               <AnimatedHeading>Madania Cafe Bar</AnimatedHeading>
               <AnimatedParagraph>An alternative cafe bistro in the area of Dafne. Take a look at its baroque design and festive atmosphere.</AnimatedParagraph>
               <Link to="/projects/4" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
-          </div> */}
+          </div>
           <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Neos Kosmos AirBnB</AnimatedHeading>
@@ -159,22 +159,22 @@ return (
             </div>
             <SampleDisplay modelPath={"/samples/sample1/index.htm"} device={"ipad"} />
           </div>
-          {/* <div className="sample-item">
+          <div className="sample-item">
             <SampleDisplay modelPath={"/projects/3"} device={"iphone"} />
             <div className="sample-info">
               <AnimatedHeading>Athens Metro Mall</AnimatedHeading>
               <AnimatedParagraph>Explore this enormous central shopping center. Have a glance at the dining and fashion options.</AnimatedParagraph>
               <Link to="/projects/3" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
-          </div> */}
-          {/* <div className="sample-item">
+          </div>
+          <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Black And White Fitness</AnimatedHeading>
               <AnimatedParagraph>Fan of fitness lifestyle? This gym offers anything you'll ever need, from pilates to TRX to weight lifting.</AnimatedParagraph>
               <Link to="/projects/3" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"/projects/2"} device={"imac"} />
-        </div> */}
+        </div>
       </div>
       <ContactInfo/>
     </div>
