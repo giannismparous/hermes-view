75b5790e5cce840c94d2dd2654df3758e28a4cbc
Giannis Mparous <p3190131@aueb.gr>
2024-04-17 01:36:37 +0300

    fixing sample page reload

--- src/components/SamplePage.jsx
+++ src/components/SamplePage.jsx
@@ -71,7 +71,7 @@ function SamplePage({ style, redirectToSample, modelPath, sampleId, ...otherProp
         </div>
       )}
       {!redirectToSample && (
-        <div style={{ ...containerStyle, ...style }} {...otherProps}>
+        <div style={{ ...containerStyle }} {...otherProps}>
           <iframe title="3D Vista Project" src={modelPath} style={iframeStyle} sandbox="allow-scripts allow-same-origin allow-top-navigation allow-popups allow-popups-to-escape-sandbox" allowFullScreen></iframe>
         </div>
       )}
