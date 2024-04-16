bacd17238e9fd22ec405d9bbb1d24d232450f43b
Giannis Mparous <p3190131@aueb.gr>
2024-04-16 23:45:49 +0300

    removing samples for debugging

--- src/components/Home.js
+++ src/components/Home.js
@@ -220,7 +220,7 @@ const AnimatedParagraph = ({ children, className }) => {
               <Link to="/projects/5" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"https://giannismparous.github.io/vr_5/"} device={"ipad"} />
-          </div>
+          </div> */}
           <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Madania Cafe Bar</AnimatedHeading>
@@ -228,7 +228,7 @@ const AnimatedParagraph = ({ children, className }) => {
               <Link to="/projects/4" className="custom-font-5" style={{ color: 'rgb(194,125,106)' }}>View Project</Link>
             </div>
             <SampleDisplay modelPath={"https://giannismparous.github.io/vr_4/"} device={"imac"} />
-          </div> */}
+          </div>
           <div className="sample-item">
             <div className="sample-info">
               <AnimatedHeading>Neos Kosmos AirBnB</AnimatedHeading>
